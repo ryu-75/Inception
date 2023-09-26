a086c4eabba0ce2ef84052a384ae33784df7bcc9
Sasha <lorion06@gmail.com>
2023-09-26 17:17:08 +0200

    light modif on mariadb-config.sh & configuration-wordpress.sh

--- srcs/requirements/mariadb/tool/mariadb-config.sh
+++ srcs/requirements/mariadb/tool/mariadb-config.sh
@@ -9,39 +9,29 @@ if [ -d /app/mysql ]; then
 else
   echo "[i] MySQL data directory not found, creating initial DBs"
 
-  mysql_install_db --user=root > /dev/null
+  mysql_install_db --user=root --basedir=/etc/mysql --datadir=/etc/mysql/data > /dev/null
 
-  if [ "$DB_ROOT_PASS" = "" ]; then
-    DB_ROOT_PASS=root
-    echo "[i] MySQL root Password: $DB_ROOT_PASS"
+  if [ "${DB_ROOT_PASS}" = "" ]; then
+    ${DB_ROOT_PASS}=root
+    echo "[i] MySQL root Password: ${DB_ROOT_PASS}"
   fi
 
   DB_DATABASE=${DB_DATABASE:-""}
   DB_USER=${DB_USER:-""}
   DB_PASS=${DB_PASS:-""}
 
-  tfile=`mktemp`
-  if [ ! -f "$tfile" ]; then
+  TMP=/etc/tmp
+  if [ ! -f ${TMP} ]; then
       return 1
   fi
 
-  cat << EOF > $tfile
-USE mysql;
-FLUSH PRIVILEGES;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$DB_ROOT_PASS" WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '';
-EOF
-
-  if [ "$DB_DATABASE" != "" ]; then
-    echo "[i] Creating database: $DB_DATABASE"
-    echo "CREATE DATABASE IF NOT EXISTS \`$DB_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
-
-    if [ "$DB_USER" != "" ]; then
-      echo "[i] Creating user: $MYSQL_USER with password $DB_PASS"
-      echo "GRANT ALL ON \`$DB_DATABASE\`.* to '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" >> $tfile
-    fi
-  fi
+  echo "CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> ${TMP}
+  echo "CREATE USER ${DB_USER} IDENTIFIED BY PASSWORD ${DB_PASS};" >> ${TMP}
+  echo "USE ${DB_DATABASE};" >> ${TMP};
+  echo "FLUSH PRIVILEGES;" >> ${TMP};
+  echo "GRANT ALL PRIVILEGES ON ${DB_DATABASE} TO ${DB_USER}.* IDENTIFIED BY "${DB_ROOT_PASS}" WITH GRANT OPTION;" >> ${TMP}
+  echo "GRANT ALL PRIVILEGES ON ${DB_DATABASE} TO '${NGIN_HOST}' WITH GRANT OPTION;" >> ${TMP}
+  echo "ALTER USER '${NGINX_HOST}' IDENTIFIED BY '${DB_USER}';" >> ${TMP}
 
   /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
   rm -f $tfile
